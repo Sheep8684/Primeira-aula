ee4cb95ec486330578180f0578f508cc29b737dd
Sheep8684 <arthurtutu2803@gmail.com>
2024-05-27 08:47:33 -0300

    Create script.js

---
+++ Linguagem de programação/2°trimestre/Lista de exercícios LP1/Exercício 2_Tema/script.js
@@ -0,0 +1,16 @@
+function mudar(){
+    let escolhaUsuário = document.getElementById('mudança').value;
+
+    switch('escolhaUsuário'){
+        case'tc':
+        function mudar(){
+        td.style.backgroundColor = "#FFFFFF"
+        }
+        break;
+        case'te':
+        function mudar(){
+        td.stylebackgroundColor = ""
+        }
+        break;
+    }
+}
